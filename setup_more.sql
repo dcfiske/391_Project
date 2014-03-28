f286cacd6db6a426ba892889cd5cce014d47ccb8
Daniel Fiske <dfiske@ualberta.ca>
2014-03-28 16:46:56 -0600

    sql for indices/seqs

---
+++ setup_more.sql
@@ -0,0 +1,5 @@
+CREATE INDEX record_desc_index ON radiology_record(description) INDEXTYPE IS CTXSYS.CONTEXT;
+CREATE INDEX record_diag_index ON radiology_record(diagnosis) INDEXTYPE IS CTXSYS.CONTEXT;
+CREATE INDEX person_fname_index ON persons(first_name) INDEXTYPE IS CTXSYS.CONTEXT;
+CREATE INDEX person_lname_index ON persons(last_name) INDEXTYPE IS CTXSYS.CONTEXT;
+commit;
