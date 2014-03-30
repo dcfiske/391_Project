9c254f7381da841db070838e6087d06c6c67ce2c
Queenie <qluc@ualberta.ca>
2014-03-30 17:07:01 -0600

    added sequence

--- setup_more.sql
+++ setup_more.sql
@@ -2,4 +2,6 @@ CREATE INDEX record_desc_index ON radiology_record(description) INDEXTYPE IS CTX
 CREATE INDEX record_diag_index ON radiology_record(diagnosis) INDEXTYPE IS CTXSYS.CONTEXT;
 CREATE INDEX person_fname_index ON persons(first_name) INDEXTYPE IS CTXSYS.CONTEXT;
 CREATE INDEX person_lname_index ON persons(last_name) INDEXTYPE IS CTXSYS.CONTEXT;
+CREATE SEQUENCE record_id_sequence;
+CREATE SEQUENCE image_id_sequence;
 commit;
